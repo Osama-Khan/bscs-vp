0edb06bb96901716b194a3e72aad33e63aa70f3d
Osama Khan <Osama6756@gmail.com>
2020-10-12 12:21:15 +0500

    Initial Commit

---
+++ ConsoleTasks/ConsoleTasks/Assignments/A1/Driver.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ * PROBLEM:
+ * Create a class called Employee that includes three pieces of information as data members,
+ * a name (type string), ID (type integer) and a monthly salary (type integer). Employee class
+ * should have a constructor that initializes the three data members. And employee class should
+ * have a destructor which shows message when object of the class is destroyed from the memory.
+ * Provide a “setData” and a “getData” function for each data member. If the monthly salary is
+ * not positive, set it to -1. Write a test program that demonstrates class Employee’s 
+ * functionalities. Create two Employee objects and display each object’s yearly salary after 
+ * deducting 4% tax.
+ * 
+ */
+
+using System;
+
+namespace ConsoleTasks.Assignments.A1
+{
+    class Driver
+    {
+        public Driver()
+        {
+            Employee e = new Employee(),
+                e2 = new Employee();
+            e.setId(0); e.setName("Ali"); e.setSalary(4000);
+            e2.setId(1); e2.setName("Junaid"); e2.setSalary(7500);
+
+            float annualSalary1 = (e.getSalary() * 12) - e.getSalary() * 12 * 0.04f;
+            float annualSalary2 = (e2.getSalary() * 12) - e2.getSalary() * 12 * 0.04f;
+
+            Console.WriteLine(e.getName() + "'s Salary: " + annualSalary1);
+            Console.WriteLine(e2.getName() + "'s Salary: " + annualSalary2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleTasks/ConsoleTasks/Assignments/A1/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleTasks.Assignments.A1
+{
+    class Employee
+    {
+        string name;
+        int id;
+        int salary;
+
+        public Employee()
+        {
+            name = "";
+            id = -1;
+            salary = -1;
+        }
+
+        ~Employee()
+        {
+            Console.WriteLine(string.Format("Employee object with id {0} destroyed", id));
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public int getSalary()
+        {
+            return salary;
+        }
+
+        public void setSalary(int salary)
+        {
+            if (salary < 0) this.salary = -1;
+            else this.salary = salary;
+        }
+    }
+}
