4273cc6e3f241327b8463beabf6d0e0159e9cfb2
Osama Khan <Osama6756@gmail.com>
2020-10-30 18:13:27 +0500

    Added Quiz2

---
+++ ConsoleTasks/ConsoleTasks/Quiz/Quiz2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTasks.Quiz
+{
+    class Quiz2
+    {
+        public Quiz2()
+        {
+            //Q1
+            Queue<string> students = new Queue<string>();
+            students.Enqueue("ALI");
+            students.Enqueue("ASAD");
+            students.Enqueue("ADEEL");
+            students.Enqueue("OMER");
+            students.Enqueue("SHAHID");
+            students.Dequeue();
+            Console.WriteLine(students.Dequeue());
+
+            //Q2
+            List<int> ages = new List<int> { 20, 25, 22, 20, 27, 29, 30, 24, 28, 19 };
+            int maxIndex = 0;
+            for (int i = 1; i < ages.Count; i++)
+            {
+                if (ages[i] > ages[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+            Console.WriteLine("Person with max age is at index: " + maxIndex);
+        }
+    }
+}
