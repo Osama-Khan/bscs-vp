ab95a604b90dc569713ee66ff59dcd1df31daf20
Osama Khan <Osama6756@gmail.com>
2020-11-25 06:18:34 +0500

    Added A7 - EditableDataGrid

---
+++ GUITasks/GUITasks/Assignments/A7/EditableDataGrid.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace GUITasks.Assignments.A7
+{
+    partial class EditableDataGrid
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_add = new System.Windows.Forms.Button();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_reg = new System.Windows.Forms.TextBox();
+            this.lbl_phone = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_reg = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.regNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(330, 65);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(100, 32);
+            this.btn_add.TabIndex = 15;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(98, 66);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(100, 20);
+            this.txt_phone.TabIndex = 14;
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(330, 28);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 20);
+            this.txt_name.TabIndex = 13;
+            // 
+            // txt_reg
+            // 
+            this.txt_reg.Location = new System.Drawing.Point(98, 28);
+            this.txt_reg.Name = "txt_reg";
+            this.txt_reg.Size = new System.Drawing.Size(100, 20);
+            this.txt_reg.TabIndex = 12;
+            // 
+            // lbl_phone
+            // 
+            this.lbl_phone.AutoSize = true;
+            this.lbl_phone.Location = new System.Drawing.Point(36, 69);
+            this.lbl_phone.Name = "lbl_phone";
+            this.lbl_phone.Size = new System.Drawing.Size(58, 13);
+            this.lbl_phone.TabIndex = 11;
+            this.lbl_phone.Text = "Phone No.";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(289, 31);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_name.TabIndex = 10;
+            this.lbl_name.Text = "Name";
+            // 
+            // lbl_reg
+            // 
+            this.lbl_reg.AutoSize = true;
+            this.lbl_reg.Location = new System.Drawing.Point(36, 31);
+            this.lbl_reg.Name = "lbl_reg";
+            this.lbl_reg.Size = new System.Drawing.Size(47, 13);
+            this.lbl_reg.TabIndex = 9;
+            this.lbl_reg.Text = "Reg No.";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.regNum,
+            this.name,
+            this.phone,
+            this.edit});
+            this.dgv.Location = new System.Drawing.Point(12, 103);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(443, 243);
+            this.dgv.TabIndex = 8;
+            // 
+            // regNum
+            // 
+            this.regNum.HeaderText = "Reg No.";
+            this.regNum.Name = "regNum";
+            this.regNum.ReadOnly = true;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // phone
+            // 
+            this.phone.HeaderText = "Phone No.";
+            this.phone.Name = "phone";
+            this.phone.ReadOnly = true;
+            // 
+            // edit
+            // 
+            this.edit.HeaderText = "Edit";
+            this.edit.Name = "edit";
+            this.edit.ReadOnly = true;
+            this.edit.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.edit.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.edit.Text = "Edit";
+            // 
+            // EditableDataGrid
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 358);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.txt_reg);
+            this.Controls.Add(this.lbl_phone);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.lbl_reg);
+            this.Controls.Add(this.dgv);
+            this.Name = "EditableDataGrid";
+            this.Text = "EditableDataGrid";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_reg;
+        private System.Windows.Forms.Label lbl_phone;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_reg;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn regNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.DataGridViewButtonColumn edit;
+    }
+}
---
+++ GUITasks/GUITasks/Assignments/A7/EditableDataGrid.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Forms;
+
+namespace GUITasks.Assignments.A7
+{
+    public partial class EditableDataGrid : Form
+    {
+        public EditableDataGrid()
+        {
+            InitializeComponent();
+            btn_add.Click += (s, e) => OnAdd();
+            dgv.CellClick += OnEdit;
+        }
+
+        private void OnAdd()
+        {
+            string reg = txt_reg.Text,
+                name = txt_name.Text,
+                phone = txt_phone.Text;
+            var row = new object[] { reg, name, phone, "Edit" };
+            dgv.Rows.Add(row);
+        }
+
+        private void OnEdit(object s, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3)
+            {
+                string reg = txt_reg.Text,
+                    name = txt_name.Text,
+                    phone = txt_phone.Text;
+                var row = e.RowIndex;
+                dgv.Rows[row].Cells[0].Value = reg;
+                dgv.Rows[row].Cells[1].Value = name;
+                dgv.Rows[row].Cells[2].Value = phone;
+            }
+        }
+    }
+}
