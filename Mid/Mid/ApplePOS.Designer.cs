37c4d4ab7240148aff102d761f158e00f0a1c0f7
Osama Khan <Osama6756@gmail.com>
2020-12-07 14:17:38 +0500

    added Q2 Designer file

---
+++ Mid/Mid/ApplePOS.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace Mid
+{
+    partial class ApplePOS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_iPhone6s_qty = new System.Windows.Forms.TextBox();
+            this.txt_iPadPro_qty = new System.Windows.Forms.TextBox();
+            this.txt_iPadMini_qty = new System.Windows.Forms.TextBox();
+            this.txt_iPod_qty = new System.Windows.Forms.TextBox();
+            this.txt_watch_qty = new System.Windows.Forms.TextBox();
+            this.txt_watch_amt = new System.Windows.Forms.TextBox();
+            this.txt_iPod_amt = new System.Windows.Forms.TextBox();
+            this.txt_iPadMini_amt = new System.Windows.Forms.TextBox();
+            this.txt_iPadPro_amt = new System.Windows.Forms.TextBox();
+            this.txt_iPhone6s_amt = new System.Windows.Forms.TextBox();
+            this.chk_iPhone6s = new System.Windows.Forms.CheckBox();
+            this.chk_iPadPro = new System.Windows.Forms.CheckBox();
+            this.chk_iPadMini = new System.Windows.Forms.CheckBox();
+            this.chk_iPod = new System.Windows.Forms.CheckBox();
+            this.chk_watch = new System.Windows.Forms.CheckBox();
+            this.txt_tax_amt = new System.Windows.Forms.TextBox();
+            this.txt_total_amt = new System.Windows.Forms.TextBox();
+            this.txt_tax = new System.Windows.Forms.TextBox();
+            this.txt_total_qty = new System.Windows.Forms.TextBox();
+            this.txt_grand_total = new System.Windows.Forms.TextBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Quantity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(273, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Amount";
+            // 
+            // txt_iPhone6s_qty
+            // 
+            this.txt_iPhone6s_qty.Location = new System.Drawing.Point(174, 25);
+            this.txt_iPhone6s_qty.Name = "txt_iPhone6s_qty";
+            this.txt_iPhone6s_qty.ReadOnly = true;
+            this.txt_iPhone6s_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_iPhone6s_qty.TabIndex = 3;
+            // 
+            // txt_iPadPro_qty
+            // 
+            this.txt_iPadPro_qty.Location = new System.Drawing.Point(174, 51);
+            this.txt_iPadPro_qty.Name = "txt_iPadPro_qty";
+            this.txt_iPadPro_qty.ReadOnly = true;
+            this.txt_iPadPro_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_iPadPro_qty.TabIndex = 4;
+            // 
+            // txt_iPadMini_qty
+            // 
+            this.txt_iPadMini_qty.Location = new System.Drawing.Point(174, 77);
+            this.txt_iPadMini_qty.Name = "txt_iPadMini_qty";
+            this.txt_iPadMini_qty.ReadOnly = true;
+            this.txt_iPadMini_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_iPadMini_qty.TabIndex = 5;
+            // 
+            // txt_iPod_qty
+            // 
+            this.txt_iPod_qty.Location = new System.Drawing.Point(174, 103);
+            this.txt_iPod_qty.Name = "txt_iPod_qty";
+            this.txt_iPod_qty.ReadOnly = true;
+            this.txt_iPod_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_iPod_qty.TabIndex = 6;
+            // 
+            // txt_watch_qty
+            // 
+            this.txt_watch_qty.Location = new System.Drawing.Point(174, 129);
+            this.txt_watch_qty.Name = "txt_watch_qty";
+            this.txt_watch_qty.ReadOnly = true;
+            this.txt_watch_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_watch_qty.TabIndex = 7;
+            // 
+            // txt_watch_amt
+            // 
+            this.txt_watch_amt.Location = new System.Drawing.Point(256, 129);
+            this.txt_watch_amt.Name = "txt_watch_amt";
+            this.txt_watch_amt.ReadOnly = true;
+            this.txt_watch_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_watch_amt.TabIndex = 12;
+            // 
+            // txt_iPod_amt
+            // 
+            this.txt_iPod_amt.Location = new System.Drawing.Point(256, 103);
+            this.txt_iPod_amt.Name = "txt_iPod_amt";
+            this.txt_iPod_amt.ReadOnly = true;
+            this.txt_iPod_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_iPod_amt.TabIndex = 11;
+            // 
+            // txt_iPadMini_amt
+            // 
+            this.txt_iPadMini_amt.Location = new System.Drawing.Point(256, 77);
+            this.txt_iPadMini_amt.Name = "txt_iPadMini_amt";
+            this.txt_iPadMini_amt.ReadOnly = true;
+            this.txt_iPadMini_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_iPadMini_amt.TabIndex = 10;
+            // 
+            // txt_iPadPro_amt
+            // 
+            this.txt_iPadPro_amt.Location = new System.Drawing.Point(256, 51);
+            this.txt_iPadPro_amt.Name = "txt_iPadPro_amt";
+            this.txt_iPadPro_amt.ReadOnly = true;
+            this.txt_iPadPro_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_iPadPro_amt.TabIndex = 9;
+            // 
+            // txt_iPhone6s_amt
+            // 
+            this.txt_iPhone6s_amt.Location = new System.Drawing.Point(256, 25);
+            this.txt_iPhone6s_amt.Name = "txt_iPhone6s_amt";
+            this.txt_iPhone6s_amt.ReadOnly = true;
+            this.txt_iPhone6s_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_iPhone6s_amt.TabIndex = 8;
+            // 
+            // chk_iPhone6s
+            // 
+            this.chk_iPhone6s.AutoSize = true;
+            this.chk_iPhone6s.Location = new System.Drawing.Point(15, 28);
+            this.chk_iPhone6s.Name = "chk_iPhone6s";
+            this.chk_iPhone6s.Size = new System.Drawing.Size(131, 17);
+            this.chk_iPhone6s.TabIndex = 13;
+            this.chk_iPhone6s.Text = "iPhone 6s - Rs 60,000";
+            this.chk_iPhone6s.UseVisualStyleBackColor = true;
+            // 
+            // chk_iPadPro
+            // 
+            this.chk_iPadPro.AutoSize = true;
+            this.chk_iPadPro.Location = new System.Drawing.Point(15, 53);
+            this.chk_iPadPro.Name = "chk_iPadPro";
+            this.chk_iPadPro.Size = new System.Drawing.Size(124, 17);
+            this.chk_iPadPro.TabIndex = 14;
+            this.chk_iPadPro.Text = "iPad Pro - Rs 80,000";
+            this.chk_iPadPro.UseVisualStyleBackColor = true;
+            // 
+            // chk_iPadMini
+            // 
+            this.chk_iPadMini.AutoSize = true;
+            this.chk_iPadMini.Location = new System.Drawing.Point(15, 79);
+            this.chk_iPadMini.Name = "chk_iPadMini";
+            this.chk_iPadMini.Size = new System.Drawing.Size(127, 17);
+            this.chk_iPadMini.TabIndex = 15;
+            this.chk_iPadMini.Text = "iPad Mini - Rs 50,000";
+            this.chk_iPadMini.UseVisualStyleBackColor = true;
+            // 
+            // chk_iPod
+            // 
+            this.chk_iPod.AutoSize = true;
+            this.chk_iPod.Location = new System.Drawing.Point(15, 105);
+            this.chk_iPod.Name = "chk_iPod";
+            this.chk_iPod.Size = new System.Drawing.Size(105, 17);
+            this.chk_iPod.TabIndex = 16;
+            this.chk_iPod.Text = "iPod - Rs 20,000";
+            this.chk_iPod.UseVisualStyleBackColor = true;
+            // 
+            // chk_watch
+            // 
+            this.chk_watch.AutoSize = true;
+            this.chk_watch.Location = new System.Drawing.Point(15, 131);
+            this.chk_watch.Name = "chk_watch";
+            this.chk_watch.Size = new System.Drawing.Size(116, 17);
+            this.chk_watch.TabIndex = 17;
+            this.chk_watch.Text = "Watch - Rs 15,000";
+            this.chk_watch.UseVisualStyleBackColor = true;
+            // 
+            // txt_tax_amt
+            // 
+            this.txt_tax_amt.Location = new System.Drawing.Point(256, 201);
+            this.txt_tax_amt.Name = "txt_tax_amt";
+            this.txt_tax_amt.ReadOnly = true;
+            this.txt_tax_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_tax_amt.TabIndex = 21;
+            // 
+            // txt_total_amt
+            // 
+            this.txt_total_amt.Location = new System.Drawing.Point(256, 175);
+            this.txt_total_amt.Name = "txt_total_amt";
+            this.txt_total_amt.ReadOnly = true;
+            this.txt_total_amt.Size = new System.Drawing.Size(84, 20);
+            this.txt_total_amt.TabIndex = 20;
+            // 
+            // txt_tax
+            // 
+            this.txt_tax.Location = new System.Drawing.Point(174, 201);
+            this.txt_tax.Name = "txt_tax";
+            this.txt_tax.ReadOnly = true;
+            this.txt_tax.Size = new System.Drawing.Size(43, 20);
+            this.txt_tax.TabIndex = 19;
+            // 
+            // txt_total_qty
+            // 
+            this.txt_total_qty.Location = new System.Drawing.Point(174, 175);
+            this.txt_total_qty.Name = "txt_total_qty";
+            this.txt_total_qty.ReadOnly = true;
+            this.txt_total_qty.Size = new System.Drawing.Size(43, 20);
+            this.txt_total_qty.TabIndex = 18;
+            // 
+            // txt_grand_total
+            // 
+            this.txt_grand_total.Location = new System.Drawing.Point(256, 227);
+            this.txt_grand_total.Name = "txt_grand_total";
+            this.txt_grand_total.ReadOnly = true;
+            this.txt_grand_total.Size = new System.Drawing.Size(84, 20);
+            this.txt_grand_total.TabIndex = 22;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(15, 278);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_calculate.TabIndex = 23;
+            this.btn_calculate.Text = "Calculate Bill";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(96, 278);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 24;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(256, 278);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(84, 23);
+            this.btn_exit.TabIndex = 25;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Total";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Tax";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 13);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Grand Total";
+            // 
+            // ApplePOS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 313);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.txt_grand_total);
+            this.Controls.Add(this.txt_tax_amt);
+            this.Controls.Add(this.txt_total_amt);
+            this.Controls.Add(this.txt_tax);
+            this.Controls.Add(this.txt_total_qty);
+            this.Controls.Add(this.chk_watch);
+            this.Controls.Add(this.chk_iPod);
+            this.Controls.Add(this.chk_iPadMini);
+            this.Controls.Add(this.chk_iPadPro);
+            this.Controls.Add(this.chk_iPhone6s);
+            this.Controls.Add(this.txt_watch_amt);
+            this.Controls.Add(this.txt_iPod_amt);
+            this.Controls.Add(this.txt_iPadMini_amt);
+            this.Controls.Add(this.txt_iPadPro_amt);
+            this.Controls.Add(this.txt_iPhone6s_amt);
+            this.Controls.Add(this.txt_watch_qty);
+            this.Controls.Add(this.txt_iPod_qty);
+            this.Controls.Add(this.txt_iPadMini_qty);
+            this.Controls.Add(this.txt_iPadPro_qty);
+            this.Controls.Add(this.txt_iPhone6s_qty);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ApplePOS";
+            this.Text = "ApplePOS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_iPhone6s_qty;
+        private System.Windows.Forms.TextBox txt_iPadPro_qty;
+        private System.Windows.Forms.TextBox txt_iPadMini_qty;
+        private System.Windows.Forms.TextBox txt_iPod_qty;
+        private System.Windows.Forms.TextBox txt_watch_qty;
+        private System.Windows.Forms.TextBox txt_iPod_amt;
+        private System.Windows.Forms.TextBox txt_iPadMini_amt;
+        private System.Windows.Forms.TextBox txt_iPadPro_amt;
+        private System.Windows.Forms.TextBox txt_iPhone6s_amt;
+        private System.Windows.Forms.CheckBox chk_iPhone6s;
+        private System.Windows.Forms.CheckBox chk_iPadPro;
+        private System.Windows.Forms.CheckBox chk_iPadMini;
+        private System.Windows.Forms.CheckBox chk_iPod;
+        private System.Windows.Forms.CheckBox chk_watch;
+        private System.Windows.Forms.TextBox txt_tax_amt;
+        private System.Windows.Forms.TextBox txt_total_amt;
+        private System.Windows.Forms.TextBox txt_tax;
+        private System.Windows.Forms.TextBox txt_total_qty;
+        private System.Windows.Forms.TextBox txt_grand_total;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_watch_amt;
+    }
+}
