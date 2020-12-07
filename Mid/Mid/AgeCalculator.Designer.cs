8ad0fef32eac44b2a65bac7c446dbc11fa9907fb
Osama Khan <Osama6756@gmail.com>
2020-12-07 14:49:02 +0500

    Added Q3

---
+++ Mid/Mid/AgeCalculator.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Mid
+{
+    partial class AgeCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.date_dob = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.lbl_days = new System.Windows.Forms.Label();
+            this.lbl_months = new System.Windows.Forms.Label();
+            this.lbl_years = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(103, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address: ";
+            // 
+            // date_dob
+            // 
+            this.date_dob.Location = new System.Drawing.Point(103, 82);
+            this.date_dob.Name = "date_dob";
+            this.date_dob.Size = new System.Drawing.Size(200, 20);
+            this.date_dob.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DOB: ";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.lbl_days);
+            this.panel1.Controls.Add(this.lbl_months);
+            this.panel1.Controls.Add(this.lbl_years);
+            this.panel1.Controls.Add(this.btn_calculate);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(103, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 140);
+            this.panel1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Age Years: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Age Months: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Age Days: ";
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(6, 94);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(187, 39);
+            this.btn_calculate.TabIndex = 3;
+            this.btn_calculate.Text = "Calculate Age";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            // 
+            // lbl_days
+            // 
+            this.lbl_days.AutoSize = true;
+            this.lbl_days.Location = new System.Drawing.Point(79, 55);
+            this.lbl_days.Name = "lbl_days";
+            this.lbl_days.Size = new System.Drawing.Size(13, 13);
+            this.lbl_days.TabIndex = 6;
+            this.lbl_days.Text = "0";
+            // 
+            // lbl_months
+            // 
+            this.lbl_months.AutoSize = true;
+            this.lbl_months.Location = new System.Drawing.Point(79, 32);
+            this.lbl_months.Name = "lbl_months";
+            this.lbl_months.Size = new System.Drawing.Size(13, 13);
+            this.lbl_months.TabIndex = 5;
+            this.lbl_months.Text = "0";
+            // 
+            // lbl_years
+            // 
+            this.lbl_years.AutoSize = true;
+            this.lbl_years.Location = new System.Drawing.Point(79, 9);
+            this.lbl_years.Name = "lbl_years";
+            this.lbl_years.Size = new System.Drawing.Size(13, 13);
+            this.lbl_years.TabIndex = 4;
+            this.lbl_years.Text = "0";
+            // 
+            // AgeCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 270);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.date_dob);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "AgeCalculator";
+            this.Text = "AgeCalculator";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker date_dob;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_days;
+        private System.Windows.Forms.Label lbl_months;
+        private System.Windows.Forms.Label lbl_years;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Mid/Mid/AgeCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using System;
+
+
+/* Question Number 3: You are required to code for GUI 
+ * given below to calculate age in years, months and 
+ * days based on given date of birth.
+ */
+namespace Mid
+{
+    public partial class AgeCalculator : Form
+    {
+        public AgeCalculator()
+        {
+            InitializeComponent();
+            btn_calculate.Click += OnCalculate;
+        }
+
+        void OnCalculate(object sender, System.EventArgs args)
+        {
+            var dob = date_dob.Value;
+            var cur = DateTime.Now;
+            var years = cur.Year - dob.Year;
+            var months = cur.Month - dob.Month;
+            var days = cur.Day - dob.Day;
+            if (months < 0) { 
+                years--;
+                months += 12;
+            }
+            if (days < 0)
+            {
+                months--;
+                days += DateTime.DaysInMonth(dob.Year, dob.Month);
+            }
+
+
+            lbl_days.Text = days + "";
+            lbl_months.Text = months + "";
+            lbl_years.Text = years + "";
+        }           
+    }
+}
