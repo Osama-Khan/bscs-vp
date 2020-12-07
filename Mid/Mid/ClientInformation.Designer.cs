da4f20561cb1050d0c7eab5b5d82526a4971f14f
Osama Khan <Osama6756@gmail.com>
2020-12-07 11:58:09 +0500

    Added Mid Q1

---
+++ Mid/Mid/ClientInformation.Designer.cs
@@ -0,0 +1,419 @@
+﻿namespace Mid
+{
+    partial class ClientInformation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.txt_state = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_zip = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_country = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.date_dob = new System.Windows.Forms.DateTimePicker();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.city = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.state = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.zip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.country = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dob = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pic_main = new System.Windows.Forms.PictureBox();
+            this.btn_choose_image = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.dialog_picture = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.25F);
+            this.label1.Location = new System.Drawing.Point(327, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client Information";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(47, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name: ";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(103, 74);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(494, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // txt_address
+            // 
+            this.txt_address.Location = new System.Drawing.Point(103, 100);
+            this.txt_address.Multiline = true;
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(494, 40);
+            this.txt_address.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(47, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Address: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label4.Location = new System.Drawing.Point(47, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "City: ";
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(103, 147);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(124, 20);
+            this.txt_city.TabIndex = 6;
+            // 
+            // txt_state
+            // 
+            this.txt_state.Location = new System.Drawing.Point(289, 147);
+            this.txt_state.Name = "txt_state";
+            this.txt_state.Size = new System.Drawing.Size(124, 20);
+            this.txt_state.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label5.Location = new System.Drawing.Point(236, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "State: ";
+            // 
+            // txt_zip
+            // 
+            this.txt_zip.Location = new System.Drawing.Point(473, 147);
+            this.txt_zip.Name = "txt_zip";
+            this.txt_zip.Size = new System.Drawing.Size(124, 20);
+            this.txt_zip.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label6.Location = new System.Drawing.Point(429, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Zip: ";
+            // 
+            // txt_country
+            // 
+            this.txt_country.Location = new System.Drawing.Point(103, 173);
+            this.txt_country.Name = "txt_country";
+            this.txt_country.Size = new System.Drawing.Size(494, 20);
+            this.txt_country.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label7.Location = new System.Drawing.Point(47, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Country: ";
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(103, 199);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(224, 20);
+            this.txt_phone.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label8.Location = new System.Drawing.Point(47, 202);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Phone: ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label9.Location = new System.Drawing.Point(333, 202);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(38, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Email: ";
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(373, 199);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(224, 20);
+            this.txt_email.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label10.Location = new System.Drawing.Point(52, 236);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(36, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "DOB: ";
+            // 
+            // date_dob
+            // 
+            this.date_dob.Location = new System.Drawing.Point(103, 230);
+            this.date_dob.Name = "date_dob";
+            this.date_dob.Size = new System.Drawing.Size(494, 20);
+            this.date_dob.TabIndex = 18;
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.address,
+            this.city,
+            this.state,
+            this.zip,
+            this.country,
+            this.phone,
+            this.email,
+            this.dob,
+            this.age});
+            this.dgv.Location = new System.Drawing.Point(50, 268);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(547, 150);
+            this.dgv.TabIndex = 19;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // address
+            // 
+            this.address.HeaderText = "Address";
+            this.address.Name = "address";
+            this.address.ReadOnly = true;
+            // 
+            // city
+            // 
+            this.city.HeaderText = "City";
+            this.city.Name = "city";
+            this.city.ReadOnly = true;
+            // 
+            // state
+            // 
+            this.state.HeaderText = "State";
+            this.state.Name = "state";
+            this.state.ReadOnly = true;
+            // 
+            // zip
+            // 
+            this.zip.HeaderText = "Zip";
+            this.zip.Name = "zip";
+            this.zip.ReadOnly = true;
+            // 
+            // country
+            // 
+            this.country.HeaderText = "Country";
+            this.country.Name = "country";
+            this.country.ReadOnly = true;
+            // 
+            // phone
+            // 
+            this.phone.HeaderText = "Phone";
+            this.phone.Name = "phone";
+            this.phone.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // dob
+            // 
+            this.dob.HeaderText = "DOB";
+            this.dob.Name = "dob";
+            this.dob.ReadOnly = true;
+            // 
+            // age
+            // 
+            this.age.HeaderText = "Age";
+            this.age.Name = "age";
+            this.age.ReadOnly = true;
+            // 
+            // pic_main
+            // 
+            this.pic_main.Location = new System.Drawing.Point(603, 74);
+            this.pic_main.Name = "pic_main";
+            this.pic_main.Size = new System.Drawing.Size(185, 145);
+            this.pic_main.TabIndex = 20;
+            this.pic_main.TabStop = false;
+            // 
+            // btn_choose_image
+            // 
+            this.btn_choose_image.Location = new System.Drawing.Point(603, 230);
+            this.btn_choose_image.Name = "btn_choose_image";
+            this.btn_choose_image.Size = new System.Drawing.Size(185, 39);
+            this.btn_choose_image.TabIndex = 21;
+            this.btn_choose_image.Text = "Choose Image";
+            this.btn_choose_image.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(603, 275);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(185, 68);
+            this.btn_save.TabIndex = 22;
+            this.btn_save.Text = "Save Data";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // dialog_picture
+            // 
+            this.dialog_picture.FileName = "picture";
+            // 
+            // ClientInformation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_choose_image);
+            this.Controls.Add(this.pic_main);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.date_dob);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_country);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt_zip);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_state);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_city);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ClientInformation";
+            this.Text = "ClientInformation";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_main)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.TextBox txt_state;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_zip;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_country;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker date_dob;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.PictureBox pic_main;
+        private System.Windows.Forms.Button btn_choose_image;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn address;
+        private System.Windows.Forms.DataGridViewTextBoxColumn city;
+        private System.Windows.Forms.DataGridViewTextBoxColumn state;
+        private System.Windows.Forms.DataGridViewTextBoxColumn zip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn country;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dob;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age;
+        private System.Windows.Forms.OpenFileDialog dialog_picture;
+    }
+}
---
+++ Mid/Mid/ClientInformation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+/* Question Number 1: GUI of client information form has been given below. 
+ * You have to write code for “Choose Image” and “Save Data” buttons. 
+ * Choose image button is used to set picture in a Picture Box while on 
+ * “Save Data” button the data of all fields must be saved in a Grid View. 
+ * Also note that age must be calculated based on date of birth and then 
+ * saved in GridView.
+ */
+namespace Mid
+{
+    public partial class ClientInformation : Form
+    {
+        public ClientInformation()
+        {
+            InitializeComponent();
+            btn_choose_image.Click += SetPicture;
+            btn_save.Click += SaveData;
+        }
+
+        void SetPicture(object sender, System.EventArgs args)
+        {
+            var res = dialog_picture.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                pic_main.Image = Image.FromFile(dialog_picture.FileName);
+            }
+        }
+
+        void SaveData(object sender, System.EventArgs args)
+        {
+            string name = txt_name.Text,
+                address = txt_address.Text,
+                city = txt_city.Text,
+                state = txt_state.Text,
+                zip = txt_zip.Text,
+                country = txt_country.Text,
+                phone = txt_phone.Text,
+                email = txt_email.Text;
+            DateTime dob = date_dob.Value;
+            DateTime cur = DateTime.Now;
+            int age = cur.Year - dob.Year;
+            if (cur.Month < dob.Month)
+            {
+                age -= 1;
+            }
+            else if (cur.Month == dob.Month)
+            {
+                age -= cur.Day < dob.Day ? 1 : 0;
+            }
+            string[] row = new string[]
+            {
+                name, address, city, state, zip, country, phone, email, dob.ToShortTimeString(), age.ToString()
+            };
+            dgv.Rows.Add(row);
+        }
+    }
+}
