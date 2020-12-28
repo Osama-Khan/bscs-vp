d26fd3ed0e4e9e99fae837643b95dbfd5948241a
Osama Khan <Osama6756@gmail.com>
2020-12-28 12:53:30 +0500

    Added Assignment 9

---
+++ GUITasks/GUITasks/Assignments/A9/ProductSearch.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace GUITasks.Assignments.A9
+{
+    partial class ProductSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.Location = new System.Drawing.Point(12, 42);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(94, 13);
+            this.lbl_search.TabIndex = 0;
+            this.lbl_search.Text = "Product ID/ Name";
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(112, 39);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(100, 20);
+            this.txt_search.TabIndex = 1;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(218, 12);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(106, 34);
+            this.btn_search.TabIndex = 2;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(218, 52);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(106, 34);
+            this.btn_update.TabIndex = 3;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 92);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(312, 210);
+            this.dgv.TabIndex = 4;
+            // 
+            // ProductSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 312);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.lbl_search);
+            this.Name = "ProductSearch";
+            this.Text = "ProductSearch";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_search;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
---
+++ GUITasks/GUITasks/Assignments/A9/ProductSearch.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GUITasks.Assignments.A9
+{
+    public partial class ProductSearch : Form
+    {
+        const string strCon = @"----";
+        DataSet ds = new DataSet();
+
+        public ProductSearch()
+        {
+            InitializeComponent();
+            btn_search.Click += (s, e) => OnSearch();
+            btn_update.Click += (s, e) => OnUpdate();
+        }
+
+        private void OnSearch()
+        {
+            var name = txt_search.Text;
+            int id;
+            bool hasId = int.TryParse(name, out id);
+
+            var conn = new SqlConnection(strCon);
+            var q = string.Format(
+                "select * from product where name like '%{0}%'", 
+                name);
+            if (hasId)
+            {
+                q += " or id = " + id;
+            }
+
+            conn.Open();
+            var cmd = new SqlCommand(q, conn);
+            var adapter = new SqlDataAdapter(cmd);
+            ds.Reset();
+            adapter.Fill(ds, "Table");
+            dgv.DataSource = ds.Tables[0];
+            conn.Close();
+        }
+
+        private void OnUpdate()
+        {
+            var conn = new SqlConnection(strCon);
+            var q = "select * from product";
+            var cmd = new SqlCommand(q, conn);
+            var adapter = new SqlDataAdapter(cmd);
+            new SqlCommandBuilder(adapter);
+            adapter.Update(ds);
+            ds.Tables[0].AcceptChanges();
+        }
+    }
+}
