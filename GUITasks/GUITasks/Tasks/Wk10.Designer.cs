21f8ca47640b6d1b276023804a53caaffbc235fe
Osama Khan <Osama6756@gmail.com>
2020-12-28 12:19:39 +0500

    Added Week 10 Lab Task

---
+++ GUITasks/GUITasks/Tasks/Wk10.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace GUITasks.Tasks
+{
+    partial class Wk10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_id = new System.Windows.Forms.TextBox();
+            this.lbl_id = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.lbl_gender = new System.Windows.Forms.Label();
+            this.txt_gender = new System.Windows.Forms.TextBox();
+            this.lbl_dob = new System.Windows.Forms.Label();
+            this.date_dob = new System.Windows.Forms.DateTimePicker();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_show_data = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_id
+            // 
+            this.txt_id.Location = new System.Drawing.Point(158, 12);
+            this.txt_id.Name = "txt_id";
+            this.txt_id.Size = new System.Drawing.Size(167, 20);
+            this.txt_id.TabIndex = 0;
+            // 
+            // lbl_id
+            // 
+            this.lbl_id.AutoSize = true;
+            this.lbl_id.Location = new System.Drawing.Point(12, 15);
+            this.lbl_id.Name = "lbl_id";
+            this.lbl_id.Size = new System.Drawing.Size(18, 13);
+            this.lbl_id.TabIndex = 1;
+            this.lbl_id.Text = "ID";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(12, 41);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_name.TabIndex = 3;
+            this.lbl_name.Text = "Name";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(158, 38);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(167, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // lbl_gender
+            // 
+            this.lbl_gender.AutoSize = true;
+            this.lbl_gender.Location = new System.Drawing.Point(12, 67);
+            this.lbl_gender.Name = "lbl_gender";
+            this.lbl_gender.Size = new System.Drawing.Size(42, 13);
+            this.lbl_gender.TabIndex = 5;
+            this.lbl_gender.Text = "Gender";
+            // 
+            // txt_gender
+            // 
+            this.txt_gender.Location = new System.Drawing.Point(158, 64);
+            this.txt_gender.Name = "txt_gender";
+            this.txt_gender.Size = new System.Drawing.Size(167, 20);
+            this.txt_gender.TabIndex = 4;
+            // 
+            // lbl_dob
+            // 
+            this.lbl_dob.AutoSize = true;
+            this.lbl_dob.Location = new System.Drawing.Point(12, 93);
+            this.lbl_dob.Name = "lbl_dob";
+            this.lbl_dob.Size = new System.Drawing.Size(27, 13);
+            this.lbl_dob.TabIndex = 7;
+            this.lbl_dob.Text = "Dob";
+            // 
+            // date_dob
+            // 
+            this.date_dob.Location = new System.Drawing.Point(158, 93);
+            this.date_dob.Name = "date_dob";
+            this.date_dob.Size = new System.Drawing.Size(167, 20);
+            this.date_dob.TabIndex = 8;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(169, 135);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // btn_show_data
+            // 
+            this.btn_show_data.Location = new System.Drawing.Point(250, 135);
+            this.btn_show_data.Name = "btn_show_data";
+            this.btn_show_data.Size = new System.Drawing.Size(75, 23);
+            this.btn_show_data.TabIndex = 10;
+            this.btn_show_data.Text = "Show Data";
+            this.btn_show_data.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 164);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(313, 278);
+            this.dgv.TabIndex = 11;
+            // 
+            // Wk10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 454);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btn_show_data);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.date_dob);
+            this.Controls.Add(this.lbl_dob);
+            this.Controls.Add(this.lbl_gender);
+            this.Controls.Add(this.txt_gender);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.lbl_id);
+            this.Controls.Add(this.txt_id);
+            this.Name = "Wk10";
+            this.Text = "Wk10";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_id;
+        private System.Windows.Forms.Label lbl_id;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.Label lbl_gender;
+        private System.Windows.Forms.TextBox txt_gender;
+        private System.Windows.Forms.Label lbl_dob;
+        private System.Windows.Forms.DateTimePicker date_dob;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_show_data;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
---
+++ GUITasks/GUITasks/Tasks/Wk10.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace GUITasks.Tasks
+{
+    public partial class Wk10 : Form
+    {
+        DataSet ds = new DataSet();
+
+        public Wk10()
+        {
+            InitializeComponent();
+            btn_show_data.Click += (s, e) => onClickShowData();
+        }
+
+        private void onClickShowData()
+        {
+            var strCon = @"----";
+            var conn = new SqlConnection(strCon);
+            conn.Open();
+            var q = "select * from student";
+            var cmd = new SqlCommand(q, conn);
+
+            var adapter = new SqlDataAdapter(cmd);
+            ds.Reset();
+            adapter.Fill(ds, "std");
+            dgv.DataSource = ds.Tables[0];
+        }
+    }
+}
