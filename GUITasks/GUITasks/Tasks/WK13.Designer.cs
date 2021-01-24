07ad6774df6ea7afb9cd34f88c955648f2ecdff4
Osama Khan <Osama6756@gmail.com>
2021-01-24 21:27:44 +0500

    Added Week 13 Labtask

---
+++ GUITasks/GUITasks/Tasks/WK13.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GUITasks.Tasks
+{
+    partial class WK13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tree = new System.Windows.Forms.TreeView();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tree
+            // 
+            this.tree.Location = new System.Drawing.Point(12, 12);
+            this.tree.Name = "tree";
+            this.tree.Size = new System.Drawing.Size(160, 433);
+            this.tree.TabIndex = 0;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(178, 12);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(175, 55);
+            this.btnGet.TabIndex = 1;
+            this.btnGet.Text = "Get Data";
+            this.btnGet.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(359, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(170, 55);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add To List";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name});
+            this.dgv.Location = new System.Drawing.Point(178, 73);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(351, 372);
+            this.dgv.TabIndex = 4;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            // 
+            // WK13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 457);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.tree);
+            this.Name = "WK13";
+            this.Text = "WK13";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tree;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+    }
+}
---
+++ GUITasks/GUITasks/Tasks/WK13.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUITasks.Tasks
+{
+    public partial class WK13 : Form
+    {
+        public WK13()
+        {
+            InitializeComponent();
+            btnGet.Click += OnButtonGetClick;
+            btnAdd.Click += OnButtonAddClick;
+        }
+
+        private void OnButtonGetClick(Object sender, EventArgs e) 
+        {
+            tree.Nodes.Add("101");
+            tree.Nodes[0].Nodes.Add("Ali");
+            tree.Nodes.Add("102");
+            tree.Nodes[1].Nodes.Add("Hashim");
+            tree.Nodes.Add("103");
+            tree.Nodes[2].Nodes.Add("Junaid");
+            tree.Nodes.Add("104");
+            tree.Nodes[3].Nodes.Add("Zain");
+        }
+
+        private void OnButtonAddClick(Object sender, EventArgs e)
+        {
+            var selected = tree.SelectedNode;
+            if (selected.Parent != null) { 
+                var c1 = selected.Parent.Text;
+                var c2 = selected.Text;
+                dgv.Rows.Add(new string[] { c1, c2 });
+            }
+            else
+            {
+                var c1 = selected.Text;
+                var c2 = selected.Nodes[0].Text;
+                dgv.Rows.Add(new string[] { c1, c2 });
+            }
+        }
+    }
+}
