2007766d06f4af4c1a3349c19244ee09cbcfeb7e
Osama Khan <Osama6756@gmail.com>
2021-02-15 18:44:25 +0500

    Added WK16 Lab task

---
+++ GUITasks/GUITasks/Tasks/WK16.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GUITasks.Tasks
+{
+    partial class WK16
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(132, 23);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(132, 59);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(22, 23);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Enter Name";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(22, 62);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(81, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Enter Password";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(132, 98);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(100, 34);
+            this.btnValidate.TabIndex = 4;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            // 
+            // WK16
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 191);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtName);
+            this.Name = "WK16";
+            this.Text = "WK16";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnValidate;
+    }
+}
---
+++ GUITasks/GUITasks/Tasks/WK16.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace GUITasks.Tasks
+{
+    public partial class WK16 : Form
+    {
+        const string nameRegex = @"^[a-zA-Z]+$"; // Only letters
+        const string passRegex = @"^(([A-z]*\d)([A-z]*))+$"; // Letters and at least one number
+
+        public WK16()
+        {
+            InitializeComponent();
+            btnValidate.Click += (s, e) => OnValidate();
+        }
+
+        void OnValidate()
+        {
+            string name = txtName.Text,
+                pass = txtPassword.Text;
+            if (Regex.IsMatch(name, nameRegex) && Regex.IsMatch(pass, passRegex))
+            {
+                MessageBox.Show("Valid pattern!");
+            } 
+            else
+            {
+                MessageBox.Show("Invalid pattern!");
+            }
+        }
+    }
+}
